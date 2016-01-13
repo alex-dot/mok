f5897c8dafc08e1a9d7fd43d973b6019a29a601e
Alexander Herr <thissideup@riseup.net>
2016-01-13 16:08:17 +0100

    fixed a typo in the database.sql

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
-CREATE TABLE users (id integer primary key, username varchar(20), passord varchar(64), salt varchar(64), token varchar(64), token_expiry integer);
 CREATE TABLE video(id INTEGER PRIMARY KEY, title VARCHAR(80), subtitle VARCHAR(80), pathname VARCHAR(120), urlname VARCHAR(60), release INTEGER, description TEXT);
 CREATE TABLE category_type (id INTEGER PRIMARY KEY, title VARCHAR(30), escaped_title VARCHAR(30), type_id SMALLINT NOT NULL DEFAULT 0);
 CREATE TABLE category_value (id INTEGER PRIMARY KEY, ctid INTEGER NOT NULL, title VARCHAR(40), escaped_title VARCHAR(40), FOREIGN KEY(ctid) REFERENCES category_type(id));
 CREATE TABLE video_category (vid INTEGER NOT NULL, cvid INTEGER NOT NULL, FOREIGN KEY(vid) REFERENCES video(id), FOREIGN KEY(cvid) REFERENCES category_value(id));
+CREATE TABLE users (id integer primary key, username varchar(20), password varchar(64), salt varchar(64), token varchar(64), token_expiry integer);
